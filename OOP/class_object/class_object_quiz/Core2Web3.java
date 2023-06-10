21c575ba6becff826296a23730d9a8893c836190
ganeshgodase4594 <godaseganesh4594@gmail.com>
2023-06-10 23:39:24 +0530

    quiz on class and object

---
+++ OOP/class_object/class_object_quiz/Core2Web3.java
@@ -0,0 +1,12 @@
+public class Core2Web3 {
+	public static void main(String[] args) {
+		System.out.println("In Core2Web");
+		Main.main(args);
+	}
+}
+class Main {
+	public static void main(String[] args) {
+		System.out.println("In Main");
+	}
+}
+
